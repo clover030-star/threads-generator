f01e41fa3f762eb3b5adc5c846922bf2085d0a0d
clover030-star <clover030@gmail.com>
2026-03-21 11:35:28 +0900

    Add files via upload

---
+++ files/sw.js
@@ -0,0 +1,7 @@
+// キャッシュを使わない（常に最新を取得）
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
